bc3b439220c20a82dc7f412ade6dd5f0b90d94dc
Bartosz Walusiak <walusiakb@gmail.com>
2019-11-29 12:43:12 +0000

    push8

--- HelloPython.py
+++ HelloPython.py
@@ -5,5 +5,3 @@ now = datetime.datetime.now()
 print("Current date and time is ")
 print(now.strftime("%A, %d-%m-%Y : %H:%M"))
 print("CIT will easily beat NCI this evening")
-
-print("It's a change!")
